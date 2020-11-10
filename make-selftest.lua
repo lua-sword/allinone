91f6d45a7b340a9ae29e98c33d0d05aabc3a2a1d
TsT <tst2005@gmail.com>
2020-11-10 19:51:57 +0100

    fix

--- make-selftest.lua
+++ make-selftest.lua
@@ -3,8 +3,8 @@ local tmpl = require "mini-tmpl"
 local eolcontrol = require "mini-tmpl.eolcontrol"
 
 do -- self test --
-	local templates = { [1] = tmpl.prepare(eolcontrol("- !{1}!{^l}\n")) }
-	local main = tmpl.prepare(eolcontrol("!{1>1}\n"))
+	local main = tmpl.prepare(eolcontrol("!{1>2}\n"))
+	local templates = { main, [2] = tmpl.prepare(eolcontrol("- !{1}!{^l}\n")) }
 	local data = {
 		{
 			"line 1",
@@ -14,6 +14,6 @@ do -- self test --
 		l="\n",
 	}
 
-	local r = tmpl.render(main, data, templates)
+	local r = tmpl.render(templates, data)
 	assert(r=="- line 1\n- line 2\n- line 3\n")
 end
